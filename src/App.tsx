ec4bb291b7ce8b8e3ac03786b1f2e7e91118ea32
Maksym Netreba <51268714+NeMaksym@users.noreply.github.com>
2025-05-12 21:48:43 +0300

    Basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css';
-
 export function App() {
-  return (
-    <>
-      <h1>Parcel React App</h1>
-      <p>Edit <code>src/App.tsx</code> to get started!</p>
-    </>
-  );
+    return (
+        <>
+            <h1>Parcel React App</h1>
+            <p>
+                Edit <code>src/App.tsx</code> to get started!
+            </p>
+        </>
+    )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
-import { createRoot } from 'react-dom/client';
-import { StrictMode } from 'react';
-import { App } from './App';
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import { App } from './App'
 
-let container = document.getElementById("app")!;
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+let container = document.getElementById('app')!
 let root = createRoot(container)
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
