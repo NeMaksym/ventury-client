d5da86f3e9ede12223e08446c6b0799b5f88ef48
Maksym Netreba <51268714+NeMaksym@users.noreply.github.com>
2025-05-14 00:04:53 +0300

    Separate layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,31 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import CssBaseline from '@mui/material/CssBaseline'
-import Box from '@mui/material/Box'
-
-import { TopBar, NavList } from './components'
+import { Layout } from './components'
 import { sideMenuPages, DEFAULT_PAGE } from './pages'
 
 export function App() {
     return (
-        <BrowserRouter>
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: '100vh',
-                    overflow: 'hidden',
-                }}
-            >
-                <CssBaseline />
-                <TopBar />
-                <Box sx={{ display: 'flex', flexGrow: 1, overflow: 'hidden' }}>
-                    <NavList />
-                    <Box
-                        component="main"
-                        sx={{
-                            flexGrow: 1,
-                            overflow: 'auto',
-                            padding: 2,
-                        }}
-                    >
-                        <Routes>
-                            {Object.entries(sideMenuPages).map(
-                                ([path, { element }]) => (
-                                    <Route
-                                        key={path}
-                                        path={path}
-                                        element={element}
-                                    />
-                                )
-                            )}
-                            <Route
-                                path="/"
-                                element={<Navigate to={DEFAULT_PAGE} replace />}
-                            />
-                        </Routes>
-                    </Box>
-                </Box>
-            </Box>
-        </BrowserRouter>
+        <>
+            <CssBaseline />
+            <BrowserRouter>
+                <Layout>
+                    <Routes>
+                        {Object.entries(sideMenuPages).map(
+                            ([path, { element }]) => (
+                                <Route
+                                    key={path}
+                                    path={path}
+                                    element={element}
+                                />
+                            )
+                        )}
+                        <Route
+                            path="/"
+                            element={<Navigate to={DEFAULT_PAGE} replace />}
+                        />
+                    </Routes>
+                </Layout>
+            </BrowserRouter>
+        </>
     )
 }
