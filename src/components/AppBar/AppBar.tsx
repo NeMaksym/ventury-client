e0ea6d6532ac3cfafe74625c7c0b25a5ad7706c4
Maksym Netreba <51268714+NeMaksym@users.noreply.github.com>
2025-05-14 00:09:04 +0300

    Add app title

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
 
 const TopBar: React.FC = () => {
     return (
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Application Title
+                    Ventury <Box component="sup" sx={{ fontSize: '0.6em', ml: -0.5 }}>Lite</Box>
                 </Typography>
             </Toolbar>
         </AppBar>
