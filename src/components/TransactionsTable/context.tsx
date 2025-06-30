8ea039eacaeba2c926933f10d7d3513a1a0e5e91
Maksym Netreba <51268714+NeMaksym@users.noreply.github.com>
2025-06-30 20:08:51 +0300

    Minor types refactoring

--- src/components/TransactionsTable/context.tsx
+++ src/components/TransactionsTable/context.tsx
@@ -1,22 +1,12 @@
 import React, { createContext, useContext, ReactNode } from 'react'
-import { TransactionActionHandler, TransactionDeleteHandler } from './types'
+import { TransactionsTableProps } from './TransactionsTable'
 
-export interface TransactionHandlers {
-    onCommentChange: TransactionActionHandler<string>
-    onCategoryChange: TransactionActionHandler<string>
-    onLabelChange: TransactionActionHandler<string[]>
-    onHideChange: TransactionActionHandler<boolean>
-    onCapitalizeChange: TransactionActionHandler<boolean>
-    onDelete: TransactionDeleteHandler
-    onSubTransactionCreate: TransactionActionHandler<number>
-}
-
-const TransactionHandlersContext = createContext<TransactionHandlers | null>(
-    null
-)
+const TransactionHandlersContext = createContext<
+    TransactionsTableProps['handlers'] | null
+>(null)
 
 interface TransactionHandlersProviderProps {
-    handlers: TransactionHandlers
+    handlers: TransactionsTableProps['handlers']
     children: ReactNode
 }
 
@@ -30,12 +20,13 @@ export const TransactionHandlersProvider: React.FC<
     )
 }
 
-export const useTransactionHandlers = (): TransactionHandlers => {
-    const context = useContext(TransactionHandlersContext)
-    if (!context) {
-        throw new Error(
-            'useTransactionHandlers must be used within a TransactionHandlersProvider'
-        )
+export const useTransactionHandlers =
+    (): TransactionsTableProps['handlers'] => {
+        const context = useContext(TransactionHandlersContext)
+        if (!context) {
+            throw new Error(
+                'useTransactionHandlers must be used within a TransactionHandlersProvider'
+            )
+        }
+        return context
     }
-    return context
-}
