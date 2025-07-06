1cfb383bdddf285d613e3fc1bf3c06a4a8e92170
Maksym Netreba <51268714+NeMaksym@users.noreply.github.com>
2025-07-07 00:48:32 +0300

    Calculate categories map

--- src/hooks/useExpenseCategories.ts
+++ src/hooks/useExpenseCategories.ts
@@ -1,12 +1,15 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 import { Category } from '../types'
 import { useExpenseCategoryService } from '../db'
+
+type CategoriesMap = Record<Category['id'], Category['label']>
 
 interface UseExpenseCategoriesReturn {
     loading: boolean
     error: string | null
     categories: Category[]
+    categoriesMap: CategoriesMap
     setCategories: React.Dispatch<React.SetStateAction<Category[]>>
 }
 
@@ -24,10 +27,21 @@ export function useExpenseCategories(): UseExpenseCategoriesReturn {
             .finally(() => setLoading(false))
     }, [getAllCategories, setCategories, setError, setLoading])
 
+    const categoriesMap = useMemo(() => {
+        const map: CategoriesMap = {}
+
+        for (const category of categories) {
+            map[category.id] = category.label
+        }
+
+        return map
+    }, [categories])
+
     return {
         loading,
         error,
         categories,
+        categoriesMap,
         setCategories,
     }
 }
